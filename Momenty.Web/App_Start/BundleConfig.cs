b0d4b3b42104c7810d383d6f08ae92122fe9efd9
Fredrik Andersson <fredrik1577@gmail.com>
2021-02-10 21:18:31 +0100

    added fund model and some config

--- Momenty.Web/App_Start/BundleConfig.cs
+++ Momenty.Web/App_Start/BundleConfig.cs
@@ -2,10 +2,9 @@
 using System.Web.Optimization;
 
 namespace Momenty.Web
-{
+{ 
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +13,6 @@ namespace Momenty.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Static/js/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Static/js/modernizr-*"));
 
---
+++ Momenty.Web/DAL/Entities/GenericFund.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Momenty.Web.DAL.Entities
+{
+    public class GenericFund
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal OneMonthReturn { get; set; }
+        public decimal ThreeMonthReturn { get; set; }
+        public decimal OneYearReturn { get; set; }
+        public DateTime DateOfScrape { get; set; }
+    }
+}
